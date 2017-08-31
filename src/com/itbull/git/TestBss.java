d2a5d40eebab6df1567eb0756ee683706619b480
tnhsp <2403931377@qq.com>
2017-09-01 00:16:09 +0800

    update eclipse

--- src/com/itbull/git/TestBss.java
+++ src/com/itbull/git/TestBss.java
@@ -5,6 +5,7 @@ public class TestBss {
 	public static void main(String[] args) {
 		System.out.println("hello bbs 100");
 		System.out.println("update by github");
-		System.out.println("update by github2");		
+		System.out.println("update by github2");
+		System.out.println("update by eclipse");
 	}
 }
